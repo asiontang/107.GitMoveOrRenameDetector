8ad9ca059161a30ae151f1dfc82ac0a98b890d3c
AsionTang <AsionTang@163.com>
2022-06-14 14:14:48 +0800

    +增加了:首个支持检测功能的代码版本

---
+++ GitMoveOrRenameDetector/MainForm.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace GitMoveOrRenameDetector
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.Label label1;
+            System.Windows.Forms.Label label2;
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnRename = new System.Windows.Forms.Button();
+            this.rtbRename = new System.Windows.Forms.RichTextBox();
+            this.btnCheck = new System.Windows.Forms.Button();
+            label1 = new System.Windows.Forms.Label();
+            label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new System.Drawing.Point(0, 0);
+            label1.Margin = new System.Windows.Forms.Padding(0);
+            label1.Name = "label1";
+            label1.Size = new System.Drawing.Size(107, 12);
+            label1.TabIndex = 1;
+            label1.Text = "被删除的文件列表:";
+            label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new System.Drawing.Point(453, 0);
+            label2.Margin = new System.Windows.Forms.Padding(0);
+            label2.Name = "label2";
+            label2.Size = new System.Drawing.Size(119, 12);
+            label2.TabIndex = 5;
+            label2.Text = "待重命名的文件列表:";
+            label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles) (((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left)));
+            this.richTextBox1.Location = new System.Drawing.Point(2, 16);
+            this.richTextBox1.Margin = new System.Windows.Forms.Padding(0);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(360, 420);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.WordWrap = false;
+            // 
+            // btnRename
+            // 
+            this.btnRename.Location = new System.Drawing.Point(365, 61);
+            this.btnRename.Name = "btnRename";
+            this.btnRename.Size = new System.Drawing.Size(85, 23);
+            this.btnRename.TabIndex = 3;
+            this.btnRename.Text = "2.确认重命名";
+            this.btnRename.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnRename.UseVisualStyleBackColor = true;
+            this.btnRename.Click += new System.EventHandler(this.btnRename_Click);
+            // 
+            // rtbRename
+            // 
+            this.rtbRename.Anchor = ((System.Windows.Forms.AnchorStyles) ((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right)));
+            this.rtbRename.Location = new System.Drawing.Point(453, 16);
+            this.rtbRename.Margin = new System.Windows.Forms.Padding(0);
+            this.rtbRename.Name = "rtbRename";
+            this.rtbRename.ReadOnly = true;
+            this.rtbRename.Size = new System.Drawing.Size(360, 420);
+            this.rtbRename.TabIndex = 4;
+            this.rtbRename.Text = "";
+            this.rtbRename.WordWrap = false;
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(365, 17);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(85, 23);
+            this.btnCheck.TabIndex = 2;
+            this.btnCheck.Text = "1.立即检测";
+            this.btnCheck.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnChcek_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(820, 439);
+            this.Controls.Add(label2);
+            this.Controls.Add(this.rtbRename);
+            this.Controls.Add(this.btnRename);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Location = new System.Drawing.Point(15, 15);
+            this.Name = "MainForm";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
+
+        private System.Windows.Forms.Button btnRename;
+        private System.Windows.Forms.RichTextBox rtbRename;
+        private System.Windows.Forms.Button btnCheck;
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+
+        #endregion
+    }
+}
---
+++ GitMoveOrRenameDetector/MainForm.cs
@@ -0,0 +1,272 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Text;
+using System.Windows.Forms;
+
+namespace GitMoveOrRenameDetector
+{
+    public partial class MainForm : Form
+    {
+        private string[] _DeletedFiles;
+        private string[] _NewFiles;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            SetProjectDirAsCurrentDirectory();
+
+            //方案1:用户在提交页面手动复制要检测重命名的文件,然后从剪贴板直接读取即可.
+            //操作步骤:用户在TortoiseGit提交页面选中OldFile和NewFile,然后Ctrl+C进行复制
+
+            //方案2:直接调用Git命令遍历当前目录,直接读取新增的文件和缺失的文件.
+            GuessGitExeFilePath();
+
+            InitData();
+        }
+
+        /// <summary>
+        /// 初始化界面数据
+        /// </summary>
+        private void InitData()
+        {
+            //CD /D "D:\2-CodingLife\我的项目\52.JS脚本库"
+
+            //获取被删除的文件名列表
+            //file:///D:/Program%20Files/Git/mingw64/share/doc/git-doc/git-diff.html
+            //"D:\Program Files\Git\cmd\git.exe" diff --diff-filter=D --name-only
+            //"D:\Program Files\Git\cmd\git.exe" ls-files --deleted
+            _DeletedFiles = GetCmdResult(GitExeFilePath, "ls-files --deleted");
+            richTextBox1.Lines = _DeletedFiles;
+
+            //显示不受版本控制的文件列表
+            //file:///D:/Program%20Files/Git/mingw64/share/doc/git-doc/git-ls-files.html
+            //"D:\Program Files\Git\cmd\git.exe" ls-files --others
+            _NewFiles = GetCmdResult(GitExeFilePath, "ls-files --others");
+        }
+
+        private static string[] GetCmdResult(string exeFilePath, string args)
+        {
+            string result;
+            using (var myProcess = new Process())
+            {
+                myProcess.StartInfo.WorkingDirectory = Environment.CurrentDirectory;
+                myProcess.StartInfo.FileName = exeFilePath;
+                myProcess.StartInfo.Arguments = args;
+
+                myProcess.StartInfo.UseShellExecute = false;
+
+                myProcess.StartInfo.StandardOutputEncoding = Encoding.UTF8;
+                myProcess.StartInfo.RedirectStandardOutput = true;
+
+                myProcess.Start();
+
+                result = myProcess.StandardOutput.ReadToEnd();
+
+                myProcess.WaitForExit();
+            }
+
+            return result.Split(new[] {'\r', '\n'}, int.MaxValue, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        #region 探测到的Git.exe文件所在位置,方便调用
+
+        /// <summary>
+        /// 探测Git.exe文件所在位置,方便调用
+        /// </summary>
+        private string GitExeFilePath { get; set; }
+
+        private void GuessGitExeFilePath()
+        {
+            var filePath = GuessGitExeFilePathInProgramFiles();
+            if (!string.IsNullOrEmpty(filePath))
+            {
+                GitExeFilePath = filePath;
+                return;
+            }
+
+            MessageBox.Show("无法探测到Git.exe程序所在位置!\n\n暂只支持固定的路径:\"?:\\Program Files\\Git\\cmd\\git.exe\"", "探测失败");
+            Environment.Exit(2);
+        }
+
+        private static string GuessGitExeFilePathInProgramFiles()
+        {
+            foreach (var driver in Environment.GetLogicalDrives())
+            {
+                foreach (var pf in new[] {"Program Files", "Program Files (x86)"})
+                {
+                    var filePath = driver + pf + @"\Git\cmd\git.exe";
+                    if (File.Exists(filePath))
+                        return filePath;
+                }
+            }
+
+            return null;
+        }
+
+        #endregion
+
+        #region 设置当前工作目录为调用者指定的目录
+
+        /// <summary>
+        /// 设置当前工作目录为调用者指定的目录
+        /// </summary>
+        private static void SetProjectDirAsCurrentDirectory()
+        {
+            var commandLineArgs = Environment.GetCommandLineArgs();
+            if (commandLineArgs.Length < 2)
+            {
+                MessageBox.Show("请传递的Git项目所在路径", "参数不全");
+                Environment.Exit(1);
+                return;
+            }
+
+            var pDir = commandLineArgs[1];
+            if (!Directory.Exists(pDir))
+            {
+                MessageBox.Show(pDir + " 不存在!", "参数不对");
+                Environment.Exit(1);
+                return;
+            }
+
+            if (!Directory.Exists(pDir))
+            {
+                MessageBox.Show(pDir + " 不存在!", "参数不对");
+                Environment.Exit(1);
+                return;
+            }
+
+            var gitDir = pDir + "\\.git";
+            if (!Directory.Exists(gitDir))
+            {
+                MessageBox.Show(pDir + " 不是正确的被Git版本控制的目录!", "目录不对");
+                Environment.Exit(1);
+                return;
+            }
+
+            //设置当前工作目录为调用者指定的目录
+            Directory.SetCurrentDirectory(pDir);
+        }
+
+        #endregion
+
+        private void btnChcek_Click(object sender, EventArgs e)
+        {
+            //遍历被删除的文件, 找到其最新的文件名
+            var delAndNewDic = new Dictionary<string, NewFileInfo>();
+            foreach (var delFileStr in _DeletedFiles)
+            {
+                var delFile = new FileInfo(delFileStr);
+
+                //遍历最新文件名, 找到相似度最高的
+                foreach (var newFileStr in _NewFiles)
+                {
+                    var newFile = new FileInfo(newFileStr);
+
+                    //更改后缀的情况.
+                    if (newFile.Extension != delFile.Extension)
+                        continue;
+
+                    //场景1: 同一个文件名,只是移动了到不同目录了.
+                    if (newFile.Name == delFile.Name)
+                    {
+                        delAndNewDic.Add(delFileStr, new NewFileInfo(newFileStr));
+                        break; //文件名一样,优先级最高.直接覆盖并返回.
+                    }
+
+                    //场景2: 同一个目录,只是重命名了
+                    if (newFile.Directory?.FullName == delFile.Directory?.FullName)
+                    {
+                        //场景3: 同一个目录,只是重命名了,并且前缀是一样的.例如Config.220101.reg -> COnfig.220202.reg
+                        var theSamePrefixCount = GetTheSamePrefixCount(newFile.Name, delFile.Name);
+                        if (theSamePrefixCount > 0)
+                        {
+                            NewFileInfo newFileInfo;
+                            if (!delAndNewDic.TryGetValue(delFileStr, out newFileInfo))
+                                delAndNewDic.Add(delFileStr, newFileInfo = new NewFileInfo());
+                            newFileInfo.AddWithTheSamePrefixCount(theSamePrefixCount, newFileStr);
+                            //NO:不能结束循环,因为可能有比它更相似的前缀存在break;
+                        }
+                    }
+                }
+            }
+
+            //将检测到重命名文件显示到界面
+            var renameList = new List<String>();
+            int i = 0;
+            foreach (var kv in delAndNewDic)
+            {
+                renameList.Add(++i + "." + kv.Key);
+                renameList.Add("└→ " + kv.Value.getNewFileStr());
+                renameList.Add("");
+            }
+
+            rtbRename.Lines = renameList.ToArray();
+        }
+
+        private class NewFileInfo
+        {
+            public NewFileInfo()
+            {
+            }
+
+            private readonly string _NewFileStr;
+
+            public NewFileInfo(string newFileStr)
+            {
+                _NewFileStr = newFileStr;
+            }
+
+            public string getNewFileStr()
+            {
+                if (_NewFileStr != null)
+                    return _NewFileStr;
+
+                //取前缀最相似的路径返回
+                KeyValuePair<int, String> maxKv = new KeyValuePair<int, string>();
+                foreach (var kv in _TheSamePrefixCountAndFileStrDic)
+                {
+                    if (kv.Key > maxKv.Key)
+                        maxKv = kv;
+                }
+
+                return maxKv.Value;
+            }
+
+            private readonly Dictionary<int, string> _TheSamePrefixCountAndFileStrDic = new Dictionary<int, string>();
+
+            public void AddWithTheSamePrefixCount(int theSamePrefixCount, string newFileStr)
+            {
+                _TheSamePrefixCountAndFileStrDic.Add(theSamePrefixCount, newFileStr);
+            }
+        }
+
+        private int GetTheSamePrefixCount(string newFileName, string delFileName)
+        {
+            if (newFileName == delFileName)
+                return newFileName.Length;
+            int TheSamePrefixCount = 0;
+            for (int i = 0; i < delFileName.Length; i++)
+            {
+                if (i >= newFileName.Length)
+                    break;
+                if (delFileName[i] == newFileName[i])
+                    TheSamePrefixCount++;
+                else
+                    return TheSamePrefixCount;
+            }
+
+            return TheSamePrefixCount;
+        }
+
+        private void btnRename_Click(object sender, EventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
